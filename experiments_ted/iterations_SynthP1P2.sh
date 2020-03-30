41336ab069cda87f68a5badd1b536da87f2c8910
Pascal Welke <welke@uni-bonn.de>
2020-03-30 08:58:30 +0200

    fixes to experiment files

--- experiments_ted/iterations_SynthP1P2.sh
+++ experiments_ted/iterations_SynthP1P2.sh
@@ -4,19 +4,20 @@
 # set. The performance of the *original* features is plotted against the
 # performance of the *topological* features.
 
+name=SynthP1P2
 
-for dataset in s60to80 s80to100 s160to180 s200to220 s200to250 s240to260; do
-  GRAPHS=../../WWL/data/${dataset}/*.gml
-  LABELS=../../WWL/data/${dataset}/Labels.txt
+for dataset in ../../WWL/${name}/*; do
+  GRAPHS=${dataset}/*.gml
+  LABELS=${dataset}/Labels.txt
   SCRIPT=../src/main.py
 
   echo
   echo $dataset
   echo "h acc_topological sdev_topological acc_original sdev_original"
 
-  for h in `seq 0 10`; do
+  for h in `seq 0 5`; do
     ACCURACY_TOP=`$SCRIPT -p 1 -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
     ACCURACY_ORG=`$SCRIPT   -u -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
     echo $h $ACCURACY_TOP $ACCURACY_ORG
   done
-done | tee egonets.results
+done | tee ${name}.results
--- experiments_ted/iterations_SynthP3.sh
+++ experiments_ted/iterations_SynthP3.sh
@@ -15,7 +15,7 @@ for dataset in ../../WWL/data/${name}/*; do
   echo $dataset
   echo "h acc_topological sdev_topological acc_original sdev_original"
 
-  for h in `seq 0 10`; do
+  for h in `seq 0 5`; do
     ACCURACY_TOP=`$SCRIPT -p 1 -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
     ACCURACY_ORG=`$SCRIPT   -u -n $h $GRAPHS -l $LABELS 2>&1 | grep Accuracy  | cut -f 2,4 -d " "`
     echo $h $ACCURACY_TOP $ACCURACY_ORG
